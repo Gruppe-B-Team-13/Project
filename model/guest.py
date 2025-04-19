cbef0e00ad7aec7d8362a3c11f849767b8f4f30f
Jan <jan.wirz@students.fhnw.ch>
2025-04-19 17:28:18 +0000

    Class guest aktualisiert first_name, last_name & loyalty_points hinzugefügt

--- model/guest.py
+++ model/guest.py
@@ -1,10 +1,12 @@
 Class Guests:
-    def __init__(self, name, email, phone_number, address, guest-id):
-        self.__name = name
+    def __init__(self, first_name, last_name, email, phone_number, address, guest-id, loyalty_points, booking_history):
+        self.__first_name = first_name
+        self.__last_name = last_name
         self.__email = email
         self.__phone_number = phone_number
         self.__address = address
-        self.__guest-id = []]
+        self.__guest-id = []
+        self.__loyalty_points = loyalty_points
 
     @property
     def name(self):
