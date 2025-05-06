419e379d920324af57fa02b443516256c7eaa8a7
Jan <jan.wirz@students.fhnw.ch>
2025-05-06 09:36:12 +0200

    Class Facilities mit Setter final

--- model/facilities.py
+++ model/facilities.py
@@ -1,7 +1,7 @@
 class Facility:
     def __init__(self, facility_id, facility_name):
-        self.__facility_id = facility_id
-        self.__facility_name = facility_name
+        self.facility_id = facility_id
+        self.facility_name = facility_name
 
     @property
     def facility_id(self):
@@ -10,6 +10,16 @@ class Facility:
     @property
     def facility_name(self):
         return self.__facility_name
+
+    @facility_id.setter
+    def facility_id(self, value):
+        self.__facility_id = value
+
+    @facility_name.setter
+    def facility_name(self, value):
+        if not value.strip():
+            raise ValueError("Der Name der Einrichtung darf nicht leer sein.")
+        self.__facility_name = value.strip()
 
     def __str__(self):
         return f"{self.__facility_name}"
