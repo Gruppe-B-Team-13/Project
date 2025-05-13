ac3b62a018044d39d296649ec6aea2fb8dc816c9
Ramiro <ramiro.gonzalezsanchez@students.fhnw.ch>
2025-05-13 20:50:56 +0000

    User Story 1.5 und 1.6 User Story 1.5 und 1.6

--- data_access/address_dal.py
+++ data_access/address_dal.py
@@ -1,33 +1,18 @@
 from data_access.base_dal import BaseDataAccess
-from data_access.address_dal import Address_DAL
-import model
+from model.address import Address
 
-class Hotel_DAL(BaseDataAccess):
+class Address_DAL(BaseDataAccess):
     def __init__(self, db_path: str = None):
         super().__init__(db_path)
-        self._address_dal = Address_DAL(db_path)   
-    
-    def get_hotel_by_id(self, hotel_id: int) -> model.Hotel | None:
-            if hotel_id is None:
-                raise ValueError("hotel_id darf nicht None sein.")
 
-            sql = """
-                SELECT hotel_id, name, stars, address_id
-                FROM   Hotel
-                WHERE  hotel_id = ?
+    def get_address_by_id(self, address_id: int) -> Address | None:
+        sql = """
+            SELECT address_id, street, house_number, zip_code, city, country FROM Address WHERE address_id = ?
             """
-            row = self.fetchone(sql, (hotel_id,))
-            if row is None:
-                return None
-
-            hotel_id, name, stars, address_id = row
-            address = self._address_dal.get_address_by_id(address_id)
-
-            if address is None:
-                raise ValueError("Fehler: Keine Adresse gefunden.")
-            return model.Hotel(
-                hotel_id=hotel_id,
-                name=name,
-                address=address,
-                stars=stars
-            )
+        params = tuple([address_id])
+        result = self.fetchone(sql, params)
+        if result:
+            address_id, street, house_number, zip_code, city, country = result
+            return Address(address_id, street, house_number, zip_code, city, country)
+        else:
+            return None
--- data_access/hotel_dal.py
+++ data_access/hotel_dal.py
@@ -1,31 +1,89 @@
-import model
 from data_access.base_dal import BaseDataAccess
 from data_access.address_dal import Address_DAL
+import model
 
 class Hotel_DAL(BaseDataAccess):
     def __init__(self, db_path: str = None):
         super().__init__(db_path)
-
+        self._address_dal = Address_DAL(db_path)   
+    
     def get_hotel_by_id(self, hotel_id: int) -> model.Hotel | None:
+        if hotel_id is None:
+            raise ValueError("hotel_id darf nicht None sein.")
+
         sql = """
-            SELECT hotel_id, name FROM Hotel WHERE hotel_id = ?
-            """
-        params = tuple([hotel_id])
-        result = self.fetchone(sql, params)
-        if result:
-            hotel_id, name = result
-            return model.Hotel(hotel_id, name)
-        else:
+            SELECT hotel_id, name, stars, address_id
+            FROM   Hotel
+            WHERE  hotel_id = ?
+        """
+        row = self.fetchone(sql, (hotel_id,))
+        if row is None:
             return None
 
-    def read_all_hotels(self) -> list[model.Hotel]:
-        sql = """
-        SELECT hotel_id, Name, stars, address_id FROM hotel
-        """
-        results = self.fetchall(sql)
-        
-        hotels = []
-        for hotel_id, name, stars, address_id in results:
-            hotel.append(model.Hotel(hotel_id=hotel_id, name=name))
+        hotel_id, name, stars, address_id = row
+        address = self._address_dal.get_address_by_id(address_id)
 
+        if address is None:
+            raise ValueError("Fehler: Keine Adresse gefunden.")
+        return model.Hotel(
+            hotel_id=hotel_id,
+            name=name,
+            address=address,
+            stars=stars
+        )
+    
+    def get_hotels_by_city(self, city: str) -> list[model.Hotel]:
+        if not city or not city.strip():
+            raise ValueError("Stadt darf nicht leer sein.")
+        
+        sql = """
+            SELECT Hotel.hotel_id,
+                Hotel.name,
+                Hotel.stars,
+                Hotel.address_id
+            FROM   Hotel
+            JOIN   Address
+            ON   Hotel.address_id = Address.address_id
+            WHERE  Address.city = ?
+        """
+        rows = self.fetchall(sql, (city.strip(),))
+        hotels: list[model.Hotel] = []
+        
+        for hotel_id, name, stars, address_id in rows:
+            address = self._address_dal.get_address_by_id(address_id)
+            hotels.append(model.Hotel(
+                hotel_id=hotel_id,
+                name=name,
+                address=address,
+                stars=stars
+            ))
+        
+        return hotels
+
+    def get_hotels_by_city and stars(self, city: str) -> list[model.Hotel]:
+        if not city or not city.strip():
+            raise ValueError("Stadt darf nicht leer sein.")
+        
+        sql = """
+            SELECT Hotel.hotel_id,
+                Hotel.name,
+                Hotel.stars,
+                Hotel.address_id
+            FROM   Hotel
+            JOIN   Address
+            ON   Hotel.address_id = Address.address_id
+            WHERE  Address.city = ?
+        """
+        rows = self.fetchall(sql, (city.strip(),))
+        hotels: list[model.Hotel] = []
+        
+        for hotel_id, name, stars, address_id in rows:
+            address = self._address_dal.get_address_by_id(address_id)
+            hotels.append(model.Hotel(
+                hotel_id=hotel_id,
+                name=name,
+                address=address,
+                stars=stars
+            ))
+        
         return hotels
