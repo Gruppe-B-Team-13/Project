156980d9e5ff5772769309cabe6b93089b766e32
Marc <marc.stoecklin@students.fhnw.ch>
2025-06-13 16:26:12 +0000

    guest_manage.py um neue Methoden zur Änderung von Vor-Nachnamen, E-Mailadresse und Adresse erweitert

--- business_logic/guest_manager.py
+++ business_logic/guest_manager.py
@@ -14,6 +14,21 @@ class GuestManager:
     def get_guest_by_id(self, guest_id: int) -> model.Guests | None:
         return self._guest_dal.get_guest_by_id(guest_id)
 
+    def change_guest_first_name(self, guest_id: int, new_first_name: str) -> bool:
+        return self.guest_dal.update_first_name(guest_id, new_first_name)
+
+    def change_guest_last_name(self, guest_id: int, new_last_name: str) -> bool:
+        return self.guest_dal.update_last_name(guest_id, new_last_name)
+
+
+    def change_guest_address_id(self, guest_id: int, new_address_id: int) -> bool:
+        return self.guest_dal.update_address_id(guest_id, new_address_id)
+
+
+    def change_guest_email(self, guest_id: int, new_email: str) -> bool:
+        return self.guest_dal.update_email(guest_id, new_email)
+
+
 
     def change_guest_phone_number(self, guest_id: int, new_phone_number: str) -> bool:
         return self.guest_dal.update_phone_number(guest_id, new_phone_number)
